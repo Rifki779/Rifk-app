b57e7baa09090e2b511241b4fa9679e2263128e6
Rifki779 <rifkisuryanahasbi@gmail.com>
2025-04-24 11:16:12 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
 st.title("LATIHAN MEMBUAT WEB MYLOVEDEZAN")
-st.write("website punya rifki nano")
+st.write("Latihan membuat website melalui Github dan Streamlit dengan menggunakan bahasa Python")
